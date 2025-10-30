baab63381b9c456c7c55453f070790c7c9012735
tentswalokhoza <tentswalokhoza2004@gmail.com>
2025-10-30 11:25:16 +0200

    Excersise 4 JS

---
+++ basics.js
@@ -0,0 +1,13 @@
+// TODO: Create a variable called firstName and store your first name in it
+var firstName = "Edward";
+// TODO: Create a variable called lastName and store your last name in it
+var lastName = "Skeletrix";
+// TODO: Create a variable called age using let (you'll modify this later)
+let age = 27;
+// TODO: Create a fullName variable that combines firstName and lastName
+var fullName = firstName + " " + lastName;
+// TODO: Print a greeting to the console using these variables
+console.log("Hello, my name is " + fullName + " and I am " + age + " years old.");
+// TODO: Try changing the age variable to a different value
+age = 31;
+console.log("Hello, my name is " + fullName + " and I am " + age + " years old.");
