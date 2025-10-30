5dd09e1a81ac60c50388890c7dbe4acc82c56859
tentswalokhoza <tentswalokhoza2004@gmail.com>
2025-10-30 12:03:49 +0200

    Exercise 2 JS

---
+++ operations-types.js
@@ -0,0 +1,15 @@
+// TODO: Create a variable called number1 and store any number in it
+var number1 = 7;
+// TODO: Create a variable called number2 and store a different number in it
+var number2 = 8;
+// TODO: Convert number1 to a string and store it in a new variable
+var number1String = number1.toString();
+// TODO: Add number1 and number2 and store the result in a variable called sum
+var sum = number1 + number2;
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+var product = number1 * number2;
+// TODO: Print all results to the console
+console.log("Number 1: " + number1);
+console.log("Number 2: " + number2);
+console.log("Sum: " + sum);
+console.log("Product: " + product);
